22b1aff0b2e09eb035e9708870837e521b5f011a
=yeri3177 <yeri3177@gmail.com>
2021-10-25 15:30:16 +0900

    admin관련 파일 수정
    1. src > admin > 서블릿,서비스,디에이오,프로퍼티즈 : 페이징내용추가
    2. adminHeader,adminFooer위치 변경함 (common패키지 -> admin패키지)
    3. wb > admin > userList.jsp : 페이징내용추가

--- src/com/kh/afm/admin/controller/AdminUserList.java
+++ src/com/kh/afm/admin/controller/AdminUserList.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.kh.afm.admin.model.service.AdminService;
+import com.kh.afm.common.MvcUtils;
 import com.kh.afm.user.model.vo.User;
 
 /**
@@ -25,21 +26,34 @@ public class AdminUserList extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		// 페이징처리
-
+		int cPage = 1;
+		int numPerPage = 10;
+		try {
+			cPage = Integer.parseInt(request.getParameter("cPage"));
+		} catch(NumberFormatException e) {
+			// 처리코드없음 
+		}
+		int startRownum = cPage * numPerPage - (numPerPage - 1);
+		int endRownum = cPage * numPerPage;
 		
 		// 업무로직
-		List<User> list = adminService.selectAllUser();
+		List<User> list = adminService.selectAllUser(startRownum, endRownum);
 		System.out.println("list@adminUserListServlet = " + list);
 		
 		// 페이징영역
+		int totalContents = adminService.selectTotalContents(); 
+		System.out.println("totalContents@servlet = " + totalContents);
+		String url = request.getRequestURI();
+		String pagebar = MvcUtils.getPagebar(cPage, numPerPage, totalContents, url);
 		
-		// 뷰단 처리
+		
+		// view단 처리
+		request.setAttribute("pagebar", pagebar);
 		request.setAttribute("list", list);
 		request
 			.getRequestDispatcher("/WEB-INF/views/admin/userList.jsp")
 			.forward(request, response);	
 		
-		
-		
+
 	}
 }
