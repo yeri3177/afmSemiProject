d5f874e4fb7da021b206ba3709692b69ae3abd8e
dawnduck07 <parkjongseo07@gmail.com>
2021-10-26 20:56:01 +0900

    csboard 업데이트!!
    csboardForm.jsp 생성
    csboardEnrollServlet.java 생성

---
+++ src/com/kh/afm/csboard/controller/CsboardEnrollServlet.java
@@ -0,0 +1,45 @@
+package com.kh.afm.csboard.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.afm.csboard.model.service.CsboardService;
+import com.kh.afm.csboard.model.vo.Csboard;
+
+/**
+ * Servlet implementation class CsboardEnrollServlet
+ */
+@WebServlet("/csboard/csboardEnroll")
+public class CsboardEnrollServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private CsboardService csboardService = new CsboardService();
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 사용자입력값 처리
+		String userId = request.getParameter("user_id");
+		String boardPassword = request.getParameter("board_password");
+		String boardTitle = request.getParameter("board_title");
+		String boardContent = request.getParameter("board_content");
+//		String boardLock = request.getParameter("board_lockYN");
+		Csboard csboard = new Csboard(0, userId, boardTitle, boardContent, null, 0, null, null, boardPassword, "Y", 0, 0, 0);
+		System.out.println("csboard@servlet = " + csboard);
+		
+		// 2. 업무로직 (db에 저장)
+		int result = csboardService.insertCsboard(csboard);
+		String msg = result > 0 ? "게시물 등록 성공!" : "게시물 등록 실패!";
+		
+		// 3. 응답 처리 : redirect(DML이기 때문에)
+		String location = request.getContextPath() + "/csboard/csboardList";
+		response.sendRedirect(location);
+	}
+
+}
--- src/com/kh/afm/csboard/model/service/CsboardService.java
+++ src/com/kh/afm/csboard/model/service/CsboardService.java
@@ -1,5 +1,41 @@
 package com.kh.afm.csboard.model.service;
 
+import static com.kh.afm.common.JdbcTemplate.*;
+import java.sql.Connection;
+import java.util.List;
+
+import com.kh.afm.csboard.model.dao.CsboardDao;
+import com.kh.afm.csboard.model.vo.Csboard;
+
 public class CsboardService {
+	
+	private CsboardDao csboardDao = new CsboardDao();
+
+	public List<Csboard> selectCsboardList(int start, int end) {
+		Connection conn = getConnection();
+		List<Csboard> list = csboardDao.selectCsboardList(conn, start, end);
+		// 단순 조회기 때문에 transaction 처리는 없다.
+		close(conn);
+		return list;
+	}
+
+	// 총 게시글 조회
+	public int selectTotalContents() {
+		Connection conn = getConnection();
+		int totalContents = csboardDao.selectTotalContents(conn);
+		close(conn);
+		return totalContents;
+	}
+
+	public int insertCsboard(Csboard csboard) {
+		Connection conn = getConnection();
+		int result = csboardDao.insertCsboard(conn, csboard);
+		// transaction처리
+		if(result > 0) commit(conn);
+		else rollback(conn);
+		
+		close(conn);
+		return result;
+	}
 
 }
