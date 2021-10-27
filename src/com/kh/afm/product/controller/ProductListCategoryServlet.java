11e632319baded796e98fba682d22b8297eb1cbd
LimGwangSu97 <hollman925@gmail.com>
2021-10-27 16:13:33 +0900

    list update

--- src/com/kh/afm/product/controller/ProductListCategoryServlet.java
+++ src/com/kh/afm/product/controller/ProductListCategoryServlet.java
@@ -1,29 +1,24 @@
 package com.kh.afm.product.controller;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.kh.afm.product.model.service.ProductService;
-
 /**
- * Servlet implementation class ProductViewServlet
+ * Servlet implementation class ProductListCategoryServlet
  */
-@WebServlet("/product/productView")
-public class ProductViewServlet extends HttpServlet {
+@WebServlet("/product/category")
+public class ProductListCategoryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	ProductService productService = new ProductService();
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		
-		
 	}
-	
+
 }
