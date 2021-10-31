f586682e6e0b2f8f5e285c6688f557835c9c5a0b
=yeri3177 <yeri3177@gmail.com>
2021-11-01 00:52:53 +0900

    구매내역 페이지 추가 (orderComplete, orderCompleteDetail 추가)
    src > order, views > order : 구매내역 관련 파일 및 내용 추가

---
+++ src/com/kh/afm/order/controller/OrderCompleteServlet.java
@@ -0,0 +1,43 @@
+package com.kh.afm.order.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.kh.afm.order.model.service.OrderService;
+import com.kh.afm.order.model.vo.Order;
+import com.kh.afm.order.model.vo.OrderDetail;
+import com.kh.afm.user.model.vo.User;
+
+/**
+ * 구매내역 (tb_order 테이블 조회)
+ */
+@WebServlet("/order/orderComplete")
+public class OrderCompleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private OrderService orderService = new OrderService();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// 로그인한 세션 정보 
+		HttpSession session = request.getSession();
+		User loginUser = (User) session.getAttribute("loginUser");
+		String userId = loginUser.getUserId();
+		System.out.println("userId = " + userId);
+		
+		// 업무로직
+		List<Order> orderList = orderService.selectOrderComplete(userId);
+		System.out.println("orderList@servlet = " + orderList);
+		
+		// view단 처리
+		request.setAttribute("orderList", orderList);
+		request.getRequestDispatcher("/WEB-INF/views/order/OrderComplete.jsp")				
+				.forward(request, response);
+	}
+}
