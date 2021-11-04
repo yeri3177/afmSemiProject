8876e0c2aabd97a42acf84473dab687199bb5206
soovening <91220553+soovening@users.noreply.github.com>
2021-11-04 12:09:10 +0900

    userAddressList.jsp 코드수정 판매자회원가입 오류 수정

--- src/com/kh/afm/user/controller/SellerEnrollServlet.java
+++ src/com/kh/afm/user/controller/SellerEnrollServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpSession;
 
 import com.kh.afm.common.MvcUtils;
 import com.kh.afm.user.model.service.UserService;
-import com.kh.afm.user.model.vo.Account;
 import com.kh.afm.user.model.vo.Address;
 import com.kh.afm.user.model.vo.User;
 
@@ -44,46 +43,57 @@ public class SellerEnrollServlet extends HttpServlet {
 		// 2.사용자입력값 -> User객체
 		String userId = request.getParameter("userId");
 		String password = MvcUtils.getEncryptedPassword(request.getParameter("password"));
-		String email = request.getParameter("email");
-		String userName = request.getParameter("userName");
+		
+		String emailId = request.getParameter("emailId");
+		String emailAddress = request.getParameter("emailAddress");
+		
+		String userName = request.getParameter("userName");	
 		String phone = request.getParameter("phone");
+		
 		String birthyear = request.getParameter("birthyear");
 		String birthmonth = request.getParameter("birthmonth");
 		String birthday = request.getParameter("birthday");
+		
 		String address1 = request.getParameter("address1");
 		String address2 = request.getParameter("address2");
-		String banknum = request.getParameter("banknum");
-		String bankname = request.getParameter("bankname");
 		
-		//생일 월 1자리이면 -> 2자리
-		if(birthmonth.length()==1) {
-			birthmonth = "0"+birthmonth;
-		}
-		//생일 일 1자리이면 -> 2자리
-		if(birthday.length()==1) {
-			birthday = "0"+birthday;
-		}
-		
-		// 생일값 3개 -> 1개
-		String s_birth = birthyear+"-"+birthmonth+"-"+birthday; // 생일 8자리 
-		
-		// 생일 String타입 -> Date타입 
-		Date d_birth = null;
-		if(s_birth != null && !"".equals(s_birth)) {
-			d_birth = Date.valueOf(s_birth);
+		//이메일값 2개 합치기 (아이디+주소)
+		System.out.println("emailId = " + emailId);
+		System.out.println("emailAddress = " + emailAddress);
+		String email = emailId+"@"+emailAddress;
+				
+		if(birthyear!=null && birthyear!=null && birthyear!=null) {
+			//생일 월 1자리이면 -> 2자리
+			if(birthmonth.length()==1) {
+				birthmonth = "0"+birthmonth;
+			}
+			//생일 일 1자리이면 -> 2자리
+			if(birthday.length()==1) {
+				birthday = "0"+birthday;
+			}
+			
+			// 생일값 3개 -> 1개
+			String s_birth = birthyear+"-"+birthmonth+"-"+birthday; // 생일 8자리 
+			
+			// 생일 String타입 -> Date타입 
+			Date d_birth = null;
+			if(s_birth != null && !"".equals(s_birth)) {
+				d_birth = Date.valueOf(s_birth);
+			}
+			
+			// User 객체 생성
+			User user = new User(userId, userName, email, password, d_birth, phone, null, UserService.SELLER_ROLE, null);
+			System.out.println("user : " + user);
+			
 		}
 
 		// User 객체 생성
-		User user = new User(userId, userName, email, password, d_birth, phone, null, UserService.SELLER_ROLE, null);
+		User user = new User(userId, userName, email, password, null, phone, null, UserService.SELLER_ROLE, null);
 		System.out.println("user : " + user);
 		
-		// Address 객체 생성
+		// Account 객체 생성
 		Address address = new Address(0, "기본주소", address1, address2, userId);
 		System.out.println("address : " + address);
-		
-		// Account 객체 생성
-		Account tb_account = new Account(0, banknum, bankname, userId);
-		System.out.println("account : " + tb_account);
 		
 		// 업무로직 (insert 처리)
 		int result = userService.insertUser(user);
@@ -91,9 +101,6 @@ public class SellerEnrollServlet extends HttpServlet {
 		
 		int result2 = userService.insertAddress(address);
 		System.out.println("insertAddress_result2 : " + result2);
-		
-		int result3 = userService.insertAccount(tb_account);
-		System.out.println("insertAccount_result3 : " + result3);
 		
 		String msg = result > 0 ? "성공적으로 회원가입했습니다." : "회원가입 실패했습니다.";
 		
